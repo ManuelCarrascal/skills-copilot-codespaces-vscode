eccec3117f2e2e2b9d24a312b3dc7a97b251da37
Manuel Carrascal <54221416+ManuelCarrascal@users.noreply.github.com>
2023-11-04 02:26:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,18 @@
+// Create web server
 
+// Create web server
+const http = require('http');
 
-// 1. Create a web server
-// 2. Create a route for GET /comments
-// 3. Read the comments from the file
-// 4. Return the comments as JSON
-// 5. Listen on port 3000
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
-
-app.get('/comments', (req, res) => {
-  fs.readFile(path.join(__dirname, 'comments.json'), (err, data) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      res.send(JSON.parse(data));
-    }
-  });
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
-
 
